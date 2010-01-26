13b64540fadd6e8a9a5d5a46fbf2707a5488cb7b
dnolen <david.nolen@gmail.com>
2010-01-26 11:21:40 -0500

    * readme.textile: edits, making scrape3.clj use nytimes.com instead of hn

--- src/tutorial/scrape3.clj
+++ src/tutorial/scrape3.clj
@@ -1,15 +1,27 @@
 (ns tutorial.scrape3
-  (:use [net.cgrand.enlive-html :as html]))
+  (:use [net.cgrand.enlive-html :as html])
+  (:use [clojure.contrib.seq-utils :only [indexed]]))
 
-(def *base-url* "http://news.ycombinator.com/")
+(def *base-url* "http://nytimes.com/")
+
+(def *nytimes-selector*
+     (html/selector #{[:h2 :a],
+                      [:h3 :a],
+                      [:ul.headlinesOnly :h5 :a],
+                      [root :> :h6 :a],
+                      [:.byline],
+                      [:.summary]}))
 
 (defn fetch-url [url]
   (html-resource (java.net.URL. url)))
 
-(defn only-domains []
-  (map html/text
-       (html/select (fetch-url *base-url*) [:td.title (but :a)])))
+(defn stories []
+  (html/select (fetch-url *base-url*) [:div.story]))
 
-(defn print-domains []
-  (doseq [line (only-domains)]
-    (println (.trim line))))
+(defn extract [node]
+  (let [result (map html/text (html/select [node] *nytimes-selector*))]
+    (zipmap [:title :byline :summary] result)))
+
+(defn empty-indices [v]
+  (filter (fn [[i s]] (nil? (seq s))) (indexed v)))
+
