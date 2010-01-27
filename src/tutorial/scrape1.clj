9bf2767c3d96fb2354b0a3a02892de9156e580f3
dnolen <david.nolen@gmail.com>
2010-01-27 02:27:03 -0500

    * src/tutorial/scrape1.clj: forgot namespacing

--- src/tutorial/scrape1.clj
+++ src/tutorial/scrape1.clj
@@ -10,7 +10,7 @@
   (map html/text (html/select (fetch-url *base-url*) [:td.title :a])))
 
 (defn hn-points []
-  (map html/text (html/select (fetch-url *base-url*) [:td.subtext first-child])))
+  (map html/text (html/select (fetch-url *base-url*) [:td.subtext html/first-child])))
 
 (defn print-headlines-and-points []
   (doseq [line (map #(str %1 " (" %2 ")") (hn-headlines) (hn-points))]
