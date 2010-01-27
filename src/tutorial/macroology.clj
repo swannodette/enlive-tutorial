ce5745872415d6dbf51d78ad362cbe079c41ee55
dnolen <david.nolen@gmail.com>
2010-01-26 20:34:56 -0500

    * src/tutorial/macroology.clj: getting warmer

--- src/tutorial/macroology.clj
+++ src/tutorial/macroology.clj
@@ -1,24 +1,23 @@
 (ns macroology
   (:require [net.cgrand.enlive-html :as html])
+  (:use [clojure.contrib.duck-streams :only [slurp*]])
+  (:use [clojure.contrib.seq-utils :only [flatten]])
   (:import [java.io ByteArrayInputStream]))
 
 (defn to-in-s [str] (ByteArrayInputStream. (.getBytes str "UTF-8")))
 
-(def *class-or-id-selector*
-     (html/selector #{[[:* (html/attr? :id)]], [[:* (html/attr? :class)]]}))
+(def *nodes-with-id*
+     (html/selector #{[[:* (html/attr? :id)]]}))
 
 (defn sel-for-node [{tag :tag, attrs :attrs}]
   (let [css-id (:id attrs)]
-    `([~(keyword (str tag "#" css-id))] (html/content ~(symbol css-id)))))
+    `([~(keyword (str (name tag) "#" css-id))] (html/content ~(symbol css-id)))))
 
 (defmacro templ [name rsrc]
-  (do
-    (println "WTF")
-   (let [nodes (select (html-resource (eval rsrc)) *class-or-id-selector*)]
-     (print nodes)
-     `(deftemplate ~name ~rsrc
-        [~'ctxt]
-        ~@(map sel-for-node nodes)))))
+  (let [nodes (select (html-resource (eval rsrc)) *class-or-id-selector*)]
+    `(deftemplate ~name (html-resource ~rsrc)
+       [~'ctxt]
+       ~@(reduce concat (map sel-for-node nodes)))))
 
 (defmacro templ-str [rsrc])
 
@@ -27,9 +26,13 @@
     [ctxt]
     [:#foo] (content (:foo ctxt)))
 
-  (templ foo *rsrcr*)
+  (templ foo *rsrc*)
 
-  (def *markup* "<span id='foo'></span><div class='bar'></div>")
+  (def *markup* "<span id='foo'></span><div class='bar'><p id='bar'></p></div>")
   (def *rsrc* (to-in-s *markup*))
-  (def *foo* (select (html-resource *rsrc*) *class-or-id-selector*))
+  (def *foo* (select (html-resource *rsrc*) *nodes-with-id*))
+  (reduce concat (map sel-for-node *foo*))
+
+  ; works
+  (map sel-for-node *foo*)
   )
