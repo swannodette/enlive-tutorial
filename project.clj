83f90d60f50f4f1ab366d9faf6b7442b8e564640
dnolen <david.nolen@gmail.com>
2010-01-26 21:47:46 -0500

    * project.clj: added lein-swank as dev-dependency, added external to verify tutorial.macroology actually works.

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "Enlive Tutorial"
   :dependencies [[enlive "1.0.0-SNAPSHOT"]
                  [compojure "0.3.2"]]
-  :dev-dependencies [[leiningen/lein-swank "1.0.0-SNAPSHOT"]])
+  :dev-dependencies [[leiningen/lein-swank "1.1.0"]]
+  :source-path "src")
---
+++ src/tutorial/external.clj
@@ -0,0 +1,11 @@
+(ns tutorial.external
+  (:use [clojure.contrib.java-utils :only [file]])
+  (:require [tutorial.macroology :as t]))
+
+(def *webdir* "/Users/davidnolen/development/clojure/enlive-tutorial/src/tutorial/")
+
+(t/templ-str foo "<span id='foo'></span><div id='bar'></div")
+(apply str (foo {:foo "Hello", :bar "world!"}))
+
+(t/templ foo (file *webdir* "introspect.html"))
+(apply str (foo {:foo "Hello", :bar "world!"}))
