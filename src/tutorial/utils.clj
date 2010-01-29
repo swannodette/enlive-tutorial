0e0a4eed502488f6346959d17eb34b6f2557d7e0
dnolen <david.nolen@gmail.com>
2010-01-28 22:37:30 -0500

    * src/tutorial/utils.clj: added parse-int to utils.clj

--- src/tutorial/utils.clj
+++ src/tutorial/utils.clj
@@ -4,6 +4,10 @@
 (defn render [t]
   (apply str t))
 
+(defmulti parse-int type)
+(defmethod parse-int java.lang.Integer [n] n)
+(defmethod parse-int java.lang.String [s] (Integer/parseInt s))
+
 (defmacro block [sym]
   `(fn [n#] (if (nil? ~sym) n# ((html/substitute ~sym) n#))))
 
