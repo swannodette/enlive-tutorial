ca86bdf1ce0aa8776bba3403914bb44efc033f32
dnolen <david.nolen@gmail.com>
2010-02-18 12:11:06 -0500

    * readme.textile: updated readme and changed scrape2.clj require -> use.

--- src/tutorial/scrape2.clj
+++ src/tutorial/scrape2.clj
@@ -1,17 +1,20 @@
 (ns tutorial.scrape2
-  (:require [net.cgrand.enlive-html :as html]))
+  (:use [net.cgrand.enlive-html :only [html-resource select text first-child]]))
 
 (def *base-url* "http://news.ycombinator.com/")
 
 (defn fetch-url [url]
-  (html/html-resource (java.net.URL. url)))
+  (html-resource (java.net.URL. url)))
 
 (defn hn-headlines-and-points []
-  (map html/text
-       (html/select (fetch-url *base-url*)
-                    #{[:td.title :a] [:td.subtext first-child]})))
+  (map text
+       (select (fetch-url *base-url*)
+               #{[:td.title :a] [:td.subtext first-child]})))
+
+(defn headline-and-points-str [[h s]]
+  (fn [[h s]] (str h " (" s ")")))
 
 (defn print-headlines-and-points []
-  (doseq [line (map (fn [[h s]] (str h " (" s ")"))
+  (doseq [line (map headline-and-points-str
                     (partition 2 (hn-headlines-and-points)))]
     (println line)))
