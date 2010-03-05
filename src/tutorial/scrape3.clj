5bbd913ca173727123b5a2c48ba5cd9bcfe84623
dnolen <david.nolen@gmail.com>
2010-03-05 18:26:44 -0500

    * src/tutorial/scrape3.clj: finally fixing up scrape3.clj

--- src/tutorial/scrape3.clj
+++ src/tutorial/scrape3.clj
@@ -1,32 +1,38 @@
 (ns tutorial.scrape3
-  (:use [net.cgrand.enlive-html :as html])
-  (:use [clojure.contrib.seq-utils :only [indexed]]))
+  (:require [net.cgrand.enlive-html :as html])
+  (:use [clojure.contrib.seq-utils :only [indexed]]
+        [clojure.contrib.str-utils :only [re-sub re-gsub]]))
 
 (def *base-url* "http://nytimes.com/")
 
+;; TODO: exclude bottom page links
+;; some of the byline stuff seems way off
+;; strip new line characters
+;; MAYBE: unicode chars
+
 (def *story-selector*
      (html/selector [[:div.story
-                      (but :.advertisement)
-                      (but :.autosStory)
-                      (but :.adCreative)]]))
+                      (html/but :.advertisement)
+                      (html/but :.autosStory)
+                      (html/but :.adCreative)]]))
 
 (def *extract-selector*
      (html/selector #{[:h2 :a],
                       [:h3 :a],
                       [:ul.headlinesOnly :h5 :a],
-                      [root :> :h6 :a],
+                      [html/root :> :h6 :a],
                       [:.byline],
                       [:.summary]}))
 
 (defn fetch-url [url]
-  (html-resource (java.net.URL. url)))
+  (html/html-resource (java.net.URL. url)))
 
 (defn stories []
   (html/select (fetch-url *base-url*) *story-selector*))
 
 (defn extract [node]
   (let [result (map html/text (html/select [node] *extract-selector*))]
-    (zipmap [:title :byline :summary] result)))
+    (zipmap [:title :byline :summary] (map #(re-gsub #"\n" "" %) result))))
 
 (defn print-story [story]
   (println)
