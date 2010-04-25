91e674b7dc4a90b7149605ce51f1b137eb7e94e4
dnolen <david.nolen@gmail.com>
2010-04-25 13:04:07 -0400

    * project.clj: changes to reflect Enlive first class selectors

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject enlive-tutorial "0.1.0"
   :description "Enlive Tutorial"
-  :dependencies [[enlive "1.0.0-dynsel-SNAPSHOT"]
+  :dependencies [[enlive "1.0.0-SNAPSHOT"]
                  [compojure "0.3.2"]]
   :dev-dependencies [[leiningen/lein-swank "1.1.0"]]
   :source-path "src")
