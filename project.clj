4d80b413d8fb6d63d8ce5b0a97916903231f2a3e
Giacomo Cosenza <giacomo.cosenza@sinapsi.com>
2013-01-10 14:50:40 +0100

    updated dependencies to last available versions

--- project.clj
+++ project.clj
@@ -1,9 +1,6 @@
 (defproject enlive-tutorial "0.1.0"
   :description "Enlive Tutorial"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [enlive "1.0.0-SNAPSHOT"]
-                 [ring "0.2.5"]
-                 [net.cgrand/moustache "1.0.0-SNAPSHOT"]]
-  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]]
-  :source-path "src")
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [enlive "1.0.1"]
+                 [ring "1.1.6"]
+                 [net.cgrand/moustache "1.1.0"]])
