b1a70b27b8dfa3304ca3aa6dd3d5679b0f5beed4
Giacomo Cosenza <giacomo.cosenza@sinapsi.com>
2013-07-15 12:23:49 +0200

    upgraded to clj 1.5.1, enlive 1.0.1 and ring 1.2.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject enlive-tutorial "0.1.0"
   :description "Enlive Tutorial"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [enlive "1.0.1"]
-                 [ring "1.1.6"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [enlive "1.1.1"]
+                 [ring "1.2.0"]
                  [net.cgrand/moustache "1.1.0"]])
